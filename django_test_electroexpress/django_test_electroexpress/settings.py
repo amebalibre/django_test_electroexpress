6b377bd6bbe2d33e3d3de03b0eb25eefdee07a15
amebalibre <vai@mailbox.org>
2019-01-30 22:05:22 +0100

    Delete comments from settings

--- django_test_electroexpress/django_test_electroexpress/settings.py
+++ django_test_electroexpress/django_test_electroexpress/settings.py
@@ -132,9 +132,3 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
-
-# SEQUENCE_FIELD_DEFAULT_EXPANDERS = (
-#     'sequence_field.expanders.NumericExpander',
-#     'sequence_field.expanders.TimeExpander',
-#     'sequence_field.expanders.ParameterExpander',
-# )
